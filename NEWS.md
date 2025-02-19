ccc9a2481dad4a8378e25f4e840e78314ba0a26c
ake123 <akeketema@yahoo.com>
2025-02-19 09:06:26 +0200

    major change

--- NEWS.md
+++
@@ -1,6 +0,0 @@
-# finna 0.1.0 (25-01-10)
-
-* Initialised app
-* Added different functionalities
-* Added unit tests
-* CRAN submission
--- R/utils.R
+++
@@ -1,4 +0,0 @@
-utils::globalVariables(c(
-  "Count", "Decade", "Language", "Library", "Series", "Subjects",
-  "Title", "author_name", "melinda_id", "response", "total"
-))
